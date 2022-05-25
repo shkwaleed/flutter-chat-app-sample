f33fbbf471d68181e6af2fbe1c3bccdc4d3cdd95
SYSTEMS\musa.ahmed <musa.ahmed@systemsltd.com>
2022-05-26 00:55:14 +0500

    flutter plugin installed. Dart Packages added.

---
+++ build/app/generated/source/buildConfig/debug/com/fiberchat/fiberchat/BuildConfig.java
@@ -0,0 +1,12 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.fiberchat.fiberchat;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "com.fiberchat.fiberchat";
+  public static final String BUILD_TYPE = "debug";
+  public static final int VERSION_CODE = 45;
+  public static final String VERSION_NAME = "1.0.44";
+}
